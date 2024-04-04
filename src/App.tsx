1d972dfe814f4a4f700c4ca2908cd2e098599bfb
Matheus <ti.matheuscunha@gmail.com>
2024-04-04 18:26:54 -0300

    all

---
+++ src/App.tsx
@@ -0,0 +1,120 @@
+import {
+  ApartmentOutlined,
+  ArrowRightOutlined,
+  ProductOutlined,
+} from "@ant-design/icons";
+import * as S from "./styles";
+import { Card } from "antd";
+import Logo from "../src/assets/carina-pires.png";
+
+const { Meta } = Card;
+
+function App() {
+  return (
+    <S.Body>
+      <S.Header>
+        <S.Container>
+          <S.Hero>
+            <S.HeroImage src={Logo} alt="Logo" />
+            <S.StyledTitle>
+              Se você foi vítima de golpes financeiros acima de R$2.500,00,
+              conheça as opções legais para pleitear a recuperação dos valores
+            </S.StyledTitle>
+            <S.StyledText>
+              A súmula 479 do STJ atribui aos Bancos a responsabilidade por
+              golpes financeiros envolvendo transferências de dinheiro entre
+              contas bancárias
+            </S.StyledText>
+            <S.StyledText>
+              Se você se encontrar nessa situação, leia o CONTEÚDO INFORMATIVO
+              DESTA PÁGINA
+            </S.StyledText>
+            <S.WhatsAppButton type="primary" href="https://wa.me/989288588">
+              FALAR DIRETAMENTE COM UM ADVOGADO
+            </S.WhatsAppButton>
+          </S.Hero>
+        </S.Container>
+      </S.Header>
+      <S.Container>
+        <S.List>
+          <S.ListItem>
+            <ArrowRightOutlined
+              style={{ color: "#25d366", fontSize: "20px" }}
+            />
+            <S.ListText>
+              Você pode ter Direito a restituir o seu dinheiro
+            </S.ListText>
+          </S.ListItem>
+          <S.ListItem>
+            <ArrowRightOutlined
+              style={{ color: "#25d366", fontSize: "20px" }}
+            />
+            <S.ListText>
+              O Banco pode ser responsabilizado, você protegido.
+            </S.ListText>
+          </S.ListItem>
+          <S.ListItem>
+            <ArrowRightOutlined
+              style={{ color: "#25d366", fontSize: "20px" }}
+            />
+            <S.ListText>Você pode ter Direito a indenização.</S.ListText>
+          </S.ListItem>
+          <S.ListItem>
+            <ArrowRightOutlined
+              style={{ color: "#25d366", fontSize: "20px" }}
+            />
+            <S.ListText>
+              Converse conosco que lhe ajudaremos a entender e lutar pelos seus
+              Direitos
+            </S.ListText>
+          </S.ListItem>
+        </S.List>
+        <S.MarketingTitle>
+          COMO PODEMOS LHE AJUDAR NESSE MOMENTO?
+        </S.MarketingTitle>
+        <S.MarketingSubtitle>
+          Assessoria Jurídica Especializada em Direito Bancário com Ênfase no
+          combate contra Fraudes Online
+        </S.MarketingSubtitle>
+        <S.CardsContainer>
+          <S.StyledCard
+            cover={
+              <ApartmentOutlined style={{ color: "red", fontSize: "50px" }} />
+            }
+          >
+            <Meta
+              title="Metodologia Estratégica:"
+              description="O nosso diferencial reside na implementação de uma metodologia estratégica completa. Antes mesmo de consultar nosso advogado, realizamos uma análise minuciosa. Esse procedimento é essencial para a coleta de provas substanciais e, assim, aumentar significativamente as probabilidades de sucesso em seu caso."
+            />
+          </S.StyledCard>
+
+          <S.StyledCard
+            cover={
+              <ProductOutlined style={{ color: "red", fontSize: "50px" }} />
+            }
+          >
+            <Meta
+              title="Advocacia Personalizada:"
+              description="Nossa abordagem é centrada na prestação de assistência personalizada, considerando fatores como empatia e resolução. Buscamos não apenas ajudar a recuperar o seu dinheiro, mas também promover o seu direito."
+            />
+          </S.StyledCard>
+        </S.CardsContainer>
+        <S.WhatsAppButton type="primary" href="https://wa.me/989288588">
+          FALAR DIRETAMENTE COM UM ADVOGADO
+        </S.WhatsAppButton>
+      </S.Container>
+      <S.Footer>
+        <S.Container>
+          <S.FooterImage src={Logo} alt="Logo" />
+          <S.FooterItem>
+            Copyright © 2024 Carina Pires - Advocacia. Todos os direitos
+            reservados.
+          </S.FooterItem>
+          <S.FooterItem>OAB/SP n° 503.201</S.FooterItem>
+        </S.Container>
+      </S.Footer>
+    </S.Body>
+  );
+}
+
+export default App;
---
+++ src/styles.tsx
@@ -0,0 +1,210 @@
+import { Button, Card } from "antd";
+import styled from "styled-components";
+
+export const Body = styled.div`
+  width: 100vw;
+`;
+
+export const Header = styled.div`
+  width: 100%;
+  height: 90vh;
+  background: #393939;
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  max-width: 1100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const Hero = styled.div`
+  height: 100%;
+  padding: 5%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: column;
+`;
+
+export const StyledText = styled.p`
+  color: #ffffff;
+  font-family: "Montserrat", Sans-serif;
+  font-size: 17px;
+  font-weight: 600;
+  letter-spacing: -0.3px;
+  text-align: center;
+  margin: 15px 0;
+
+  @media screen and (max-width: 768px) {
+    font-size: 14px;
+  }
+`;
+
+export const StyledTitle = styled.h1`
+  color: #ffffff;
+  font-family: "Montserrat", Sans-serif;
+  font-size: 39px;
+  font-weight: 600;
+  letter-spacing: -0.3px;
+  text-align: center;
+  margin-top: 15px;
+
+  @media screen and (max-width: 768px) {
+    font-size: 22px;
+  }
+`;
+
+export const HeroImage = styled.img`
+  max-width: 300px;
+  max-height: 300px;
+  object-fit: contain;
+
+  @media screen and (max-width: 768px) {
+    fmax-width: 200px;
+    max-height: 200px;
+  }
+`;
+
+export const WhatsAppButton = styled(Button)`
+  width: 500px;
+  height: 50px;
+  background: #25d366 !important;
+  border-color: #25d366 !important;
+  font-weight: 700;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    background: #075e54 !important;
+    border-color: #075e54 !important;
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 300px;
+    font-size: 12px;
+  }
+`;
+
+export const List = styled.div`
+  width: 70%;
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  background: #f6f6f6;
+  border-radius: 25px;
+  margin: 40px auto;
+  justify-content: center;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+export const ListItem = styled.div`
+  display: flex;
+  padding: 2%;
+`;
+
+export const ListText = styled.p`
+  font-family: "Montserrat", Sans-serif;
+  font-size: 17px;
+  font-weight: 500;
+  margin-left: 10px;
+`;
+
+export const MarketingTitle = styled.p`
+  color: #000000;
+  font-family: "Montserrat", Sans-serif;
+  font-size: 58px;
+  font-weight: 300;
+  line-height: 47px;
+  letter-spacing: 0px;
+  text-align: center;
+  margin: 10px auto;
+  max-width: 900px;
+
+  @media screen and (max-width: 768px) {
+    font-size: 32px;
+    margin-top: 40px;
+    padding: 5%;
+  }
+`;
+
+export const MarketingSubtitle = styled.p`
+  color: #000000;
+  font-family: "Montserrat", Sans-serif;
+  font-size: 26px;
+  font-weight: 300;
+  letter-spacing: 0px;
+  text-align: center;
+  margin: 10px auto;
+  max-width: 900px;
+
+  @media screen and (max-width: 768px) {
+    font-size: 20px;
+    padding: 0 5% 5% 5%;
+    margin: 0;
+  }
+`;
+
+export const StyledCard = styled(Card)`
+  width: 400px;
+  padding: 20px;
+
+  .ant-card-meta-title {
+    text-align: center;
+  }
+
+  .ant-card-meta-description {
+    text-align: center;
+  }
+
+  @media screen and (max-width: 768px) {
+    max-width: 100%;
+    border-radius: 0;
+  }
+`;
+
+export const CardsContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin: 40px 0;
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+export const Footer = styled.div`
+  width: 100%;
+  height: 150px;
+  background: #393939;
+  display: flex;
+  align-items: center;
+  margin-top: 40px;
+
+  > div {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+export const FooterItem = styled.div`
+  color: #ffffff;
+  font-family: "Montserrat", Sans-serif;
+  font-size: 17px;
+  font-weight: 600;
+  letter-spacing: -0.3px;
+  text-align: center;
+`;
+
+export const FooterImage = styled.img`
+  width: 60px;
+  height: 60px;
+  object-fit: contain;
+`;
